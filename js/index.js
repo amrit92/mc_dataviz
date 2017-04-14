abae7820139a895cf0a76f305dc6c7c0148682ae
amrit <amritsahoo@gmail.com>
2017-04-14 12:28:16 -0700

    update msg

--- js/index.js
+++ js/index.js
@@ -37,66 +37,7 @@ function myFunction(i) {
     };
   return trace
 }
-// var trace1 = {
-//   x: unpack(rows, 'x1'),
-//   y: unpack(rows, 'y1'),
-//   z: unpack(rows, 'z1'),
-//   mode: 'lines',
-//   marker: {
-//     color: '#1f77b4',
-//     size: 12,
-//     symbol: 'circle',
-//     line: {
-//       color: 'rgb(0,0,0)',
-//       width: 0
-//     }
-//   },
-//   line: {
-//     color: '#1f77b4',
-//     width: 1
-//   },
-//   type: 'scatter3d'
-// };
-// var trace2 = {
-//   x: unpack(rows, 'x2'),
-//   y: unpack(rows, 'y2'),
-//   z: unpack(rows, 'z2'),
-//   mode: 'lines',
-//   marker: {
-//     color: '#9467bd',
-//     size: 12,
-//     symbol: 'circle',
-//     line: {
-//       color: 'rgb(0,0,0)',
-//       width: 0
-//     }
-//   },
-//   line: {
-//     color: 'rgb(44, 160, 44)',
-//     width: 1
-//   },
-//   type: 'scatter3d'
-// };
-// var trace3 = {
-//   x: unpack(rows, 'x3'),
-//   y: unpack(rows, 'y3'),
-//   z: unpack(rows, 'z3'),
-//   mode: 'lines',
-//   marker: {
-//     color: '#bcbd22',
-//     size: 12,
-//     symbol: 'circle',
-//     line: {
-//       color: 'rgb(0,0,0)',
-//       width: 0
-//     }
-//   },
-//   line: {
-//     color: '#bcbd22',
-//     width: 1
-//   },
-//   type: 'scatter3d'
-// };
+
 
 array = new Array();
 for (i = 0; i < 61; i++) {
